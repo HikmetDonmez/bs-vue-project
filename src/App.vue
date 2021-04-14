fbadb87e5dcfc53e61fb56db1cc0b2fa281f2c53
HikmetDonmez <61684718+HikmetDonmez@users.noreply.github.com>
2021-04-14 14:32:23 +0300

    added-user-page
    Added user page

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <div>
-      Test
-    </div>
+    <user-page />
   </div>
 </template>
 
 <script>
-
+import UserPage from './components/userPage.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    UserPage
   }
 }
 </script>
 
+<style>
+@import url("https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css");
+</style>
